8d6ef54d220a8b3c458eeab9dbf0246230ed29d1
Avichai Ben David <avbendav@microsoft.com>
2017-12-31 11:25:29 +0200

    euler 21 extended

--- euler_21.py
+++ euler_21.py
@@ -10,8 +10,8 @@
 # Evaluate the sum of all the amicable numbers under 10000.
 
 # Avichai Ben David
+import math
 
-set
 LIM = 10000
 
 
@@ -19,11 +19,29 @@ def sum_div_of_num(num):
     return sum(j for j in range(1, num // 2 + 1) if num % j == 0)
 
 
+def sum_div_of_num_efficient(num):
+    if num == 1:
+        return 0
+    else:
+        r = math.floor(math.sqrt(num))
+        res = 1 + r if r * r == num else 1
+        if num % 2 != 0:
+            f = 3
+            step = 2
+        else:
+            f = 2
+            step = 1
+        while f < r:
+            res += (f + num // f) if num % f == 0 else 0
+            f += step
+        return res
+
+
 def compute():
     amicable_set = set()
     sum_divisors = {}
     for num in range(2, LIM):
-        sum_divisors[num] = sum_div_of_num(num)
+        sum_divisors[num] = sum_div_of_num_efficient(num)
     for key_1 in sum_divisors.keys():
         for key_2 in sum_divisors.keys():
             if sum_divisors[key_1] == key_2 and sum_divisors[key_2] == key_1 and key_1 != key_2:
@@ -46,6 +64,16 @@ def compute2():
     return res
 
 
+def compute3():
+    res = 0
+    for num_1 in range(2, LIM):
+        num_2 = sum_div_of_num_efficient(num_1)
+        if num_2 > num_1 and sum_div_of_num_efficient(num_2) == num_1:
+            res += num_1 + num_2
+    return res
+
+
 if __name__ == "__main__":
-    print(compute2()) # much more efficient
-    # print(compute())
+    print(compute2())  # much more efficient
+    print(compute3())
+    print(compute())
